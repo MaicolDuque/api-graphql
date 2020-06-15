3be8bdbb7c33ee32e0781e03cb03a6e14e5aa6ed
Maicol Felipe Duque Urrea <maicolduque01@gmail.com>
2020-06-15 00:45:16 -0500

    Use bcrypt to save password in database

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
   email: String,
@@ -11,6 +12,19 @@ const userSchema = new Schema({
     type: Schema.Types.ObjectId, // reference every ID of your courses
     ref: 'Course'
   }]
+});
+
+userSchema.virtual('password'); // Se crear virtual, para que no se almacena esta info en base de datos, solo cifrada
+
+userSchema.pre('save', async function(){ // Hook que se ejecutará antes de guardar un nuevo usuario en la BD.
+  if(this.password === undefined) return;
+  try {
+    const hash = await bcrypt.hash(this.password, 10);    
+    this.hashedPassword = hash;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 });
 
 module.exports = model('User', userSchema);
